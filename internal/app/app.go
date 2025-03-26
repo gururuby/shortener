ec4beecf1ab700d413286bd4180cd078a4b0f22f
V.Miroshnichenko <i@gururuby.ru>
2025-03-26 13:30:51 +0300

    fix(Sprint1): Move configuration parsing to config package

--- internal/app/app.go
+++ internal/app/app.go
@@ -1,8 +1,6 @@
 package app
 
 import (
-	"flag"
-	"github.com/caarlos0/env/v6"
 	appConfig "github.com/gururuby/shortener/internal/config"
 	"github.com/gururuby/shortener/internal/repos"
 	"github.com/gururuby/shortener/internal/router"
@@ -12,27 +10,6 @@ import (
 
 func Run() {
 	config := appConfig.NewConfig()
-	// Override config from ENVs
-	err := env.Parse(config)
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	// Override config via flags
-	flag.StringVar(&config.ServerAddress, "a", "", "Server address")
-	flag.StringVar(&config.BaseURL, "b", "", "Base URL of short URLs")
-
-	flag.Parse()
-
-	// Setup default config values
-	if config.ServerAddress == "" {
-		config.ServerAddress = "localhost:8080"
-	}
-
-	if config.BaseURL == "" {
-		config.BaseURL = "http://localhost:8080"
-	}
-
 	storage := repos.NewShortURLsRepo()
 
 	log.Fatal(http.ListenAndServe(config.ServerAddress, router.NewRouter(config, storage)))
--- internal/config/config.go
+++ internal/config/config.go
@@ -1,13 +1,30 @@
 package config
 
+import (
+	"flag"
+	"github.com/caarlos0/env/v6"
+	"log"
+)
+
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS"`
 	BaseURL       string `env:"BASE_URL"`
 }
 
 func NewConfig() *Config {
-	return &Config{
-		ServerAddress: "localhost:8080",
-		BaseURL:       "http://localhost:8080",
+	var config Config
+
+	// Override config from ENVs
+	err := env.Parse(&config)
+	if err != nil {
+		log.Fatal(err)
 	}
+
+	// Override config via flags
+	flag.StringVar(&config.ServerAddress, "a", "localhost:8080", "Server address")
+	flag.StringVar(&config.BaseURL, "b", "http://localhost:8080", "Base URL of short URLs")
+
+	flag.Parse()
+
+	return &config
 }
