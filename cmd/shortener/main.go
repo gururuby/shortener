7736ccc87c75074c6f1bdc50b60836747001ed78
Vladimir <i@gururuby.ru>
2025-07-28 10:44:08 +0300

    feat(Iter20): Add more tests, add build info logging

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -21,6 +21,14 @@ import (
 	"log"
 )
 
+// Global variables storing build information.
+// These are set during the build process using ldflags.
+var (
+	buildVersion string // Version number of the build
+	buildDate    string // Date when the build was created
+	buildCommit  string // Git commit hash of the build
+)
+
 // main is the application entry point.
 //
 // It performs:
@@ -30,9 +38,29 @@ import (
 //
 // If any step fails, it logs the error and terminates.
 func main() {
+	logBuildInfo()
 	cfg, err := config.New()
 	if err != nil {
 		log.Fatalf("cannot setup config: %s", err)
 	}
 	app.New(cfg).Setup().Run()
+}
+
+// logBuildInfo logs the build version, date and commit information.
+// If any build information is empty, it will be displayed as "N/A".
+func logBuildInfo() {
+	log.Printf("Build version: %s", handleBuildValue(buildVersion))
+	log.Printf("Build date: %s", handleBuildValue(buildDate))
+	log.Printf("Build commit: %s", handleBuildValue(buildCommit))
+}
+
+// handleBuildValue returns "N/A" if the input string is empty,
+// otherwise returns the string itself.
+//
+// This is used to handle unset build variables gracefully.
+func handleBuildValue(v string) string {
+	if v == "" {
+		return "N/A"
+	}
+	return v
 }
